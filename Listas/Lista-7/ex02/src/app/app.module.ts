ed50d0b4e666314f0c203a4820f6c798ccdc21c5
PaulinhoLeal <paulonagasumileal@gmail.com>
2023-08-25 00:38:12 -0300

    Consertando lista 7  angular-material

--- Listas/Lista-7/ex02/src/app/app.module.ts
+++ Listas/Lista-7/ex02/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
@@ -27,7 +27,8 @@ import {MatInputModule} from '@angular/material/input';
     BrowserAnimationsModule,
     MatIconModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
 
   ],
   providers: [],
